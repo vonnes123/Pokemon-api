1f70583a1cf91a1e89578bb8d86cfea7b914f912
Vasil <vasil.todorov@student.fontys.nl>
2022-09-30 16:06:00 +0200

    add get img, name, id, abilities

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-fetch("https://pokeapi.co/api/v2/pokemon-species/aegislash")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
+for(let x = 0; x <= 9; x++){
+    getAPI(x);
+    console.log(x)
+}
+
+function getAPI(pokemonNum){
+    let randomPokemon = Math.floor(Math.random() * 800) + 1;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+        .then((res) => res.json())
+        .then((data) => {
+        //console.log(data);
+        console.log(pokemonNum);
+        document.getElementById(`pokemon-img${pokemonNum}`).style.backgroundImage = `url(${data.sprites.front_default})`;
+        //console.log(data.name)
+        document.getElementById(`name${pokemonNum}`).innerHTML = data.name;
+        document.getElementById(`id${pokemonNum}`).innerHTML = data.id;
+        document.getElementById(`Ability${pokemonNum}-1`).innerHTML = data.abilities[0].ability.name;
+        //console.log(data.abilities.length);
+        if (data.abilities.length >= 2) {
+          document.getElementById(`Ability${pokemonNum}-2`).innerHTML =
+            data.abilities[1].ability.name;
+        } else {
+          document.getElementById(`Ability${pokemonNum}-2`).innerHTML = "";
+        }
+
+    });
+}
